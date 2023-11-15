55a753f7e84638aef33cc3a315b86171834e30d5
Lou Fogola <lou.fogola@gmail.com>
2023-11-15 17:01:06 +0100

    modified:   bot.js

---
+++ bot.js
@@ -0,0 +1,23 @@
+//Fichier d'execution du scripte du bot
+import './index.css'
+
+//Création des Constantes utilisées par le bot
+const form  = document.getElementsByClassName('cell')
+is_Dispo()
+
+//Fonction de vérification si la case est disponible
+function is_Dispo(){
+    console.log(form)
+}
+
+
+//Fonction de choix aléatoir du positionnement de l'element
+function drop_Element_alea(){
+
+}
+
+
+//Fonction Positionnement de l'élément
+function placement_Element(){
+
+}
