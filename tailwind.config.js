09c6aac74a51962bc343f6dc251495caa1a7cc2c
tprat <Titouan.p@hotmail.fr>
2023-11-15 16:42:05 +0100

    html ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'default': ['Lilita One', 'Roboto', 'sans-serif'],
+    },
+    colors: {
+      'text': '#EAE9FC',
+      'background': {
+        'primary': '#0A072C',
+        'secondary': '#100B45',
+      },
+      'black': '#000000',
+    },
   },
   plugins: [],
 }
