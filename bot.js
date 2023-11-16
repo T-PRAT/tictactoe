6f0db90437dc6c6eb68fdcc4f10bef810289c4ae
Lou Fogola <lou.fogola@gmail.com>
2023-11-16 10:12:00 +0100

    modified:   bot.js

--- bot.js
+++ bot.js
@@ -2,26 +2,47 @@
 
 //Création des Constantes utilisées par le bot
 const cell  = document.querySelectorAll('.cell')
+
+let tablueDeJeu = ["", "", "", "", "", "", "", "", ""];
+
+// Combinaisons possibles pour gagner
+const merciGoogle = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+const formeBot = "O";
 is_Dispo()
 
 //Fonction de vérification si la case est disponible
-function is_Dispo(){
-    cell.forEach(cellules => {
-        var cellActive = cellules.childNodes
-        cellActive.forEach(element=>{
-            console.log(element.firstChild)//.parentNode.childNodes
-        })
-    })
+function is_Dispo(emplacementTableJeux){
+    if(tablueDeJeu[emplacementTableJeux] == ''){
+        return true
+    }
+    else{
+        return false
+    }
 }
 
 
 //Fonction de choix aléatoir du positionnement de l'element
 function drop_Element_alea(){
-
+    caseJouee = Math.random() * tablueDeJeu.length
+    if(is_Dispo(caseJouee)){
+        placement_Element(caseJouee)
+    }
 }
 
 
 //Fonction Positionnement de l'élément
-function placement_Element(){
-
+function placement_Element(caseJouee){
+    tablueDeJeu[caseJouee] = formeBot
+    cell[caseJouee].innerHTML = formeBot
 }
+
+exporte.module()
